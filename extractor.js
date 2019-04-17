b6e5f53b46502c5bb8296dd429da0c6e66cde2d6
lgcanto <lucascanto@poli.ufrj.br>
2019-04-17 19:19:07 -0300

    Converting xml to json

--- extractor.js
+++ extractor.js
@@ -1,8 +1,14 @@
-var source_api = 'http://rss.uol.com.br/feed/economia.xml';
+// var source_api = 'http://rss.uol.com.br/feed/economia.xml';
+var requestOptions  = { encoding: 'binary', method: 'GET', uri: 'http://rss.uol.com.br/feed/economia.xml'};
 var parser = require('fast-xml-parser');
 
 var request = require('request');
-request(source_api, function (error, response, body) {
+request(requestOptions, function (error, response, body) {
   var json = parser.parse(body);
   var newsArray = json['rss']['channel']['item'];
+  newsArray.forEach(function(news){ 
+    delete news.link; 
+    delete news.pubDate;
+    news.description = news.description.replace(/<img .*?>/g, '');
+  });
 });
