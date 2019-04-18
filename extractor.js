1fb7e492c6b6e9f475590b3f7a7154b226eef007
lgcanto <lucascanto@poli.ufrj.br>
2019-04-17 22:49:04 -0300

    Converting news to .csv

--- extractor.js
+++ extractor.js
@@ -1,14 +1,21 @@
-// var source_api = 'http://rss.uol.com.br/feed/economia.xml';
-var requestOptions  = { encoding: 'binary', method: 'GET', uri: 'http://rss.uol.com.br/feed/economia.xml'};
-var parser = require('fast-xml-parser');
+const requestOptions  = { encoding: 'binary', method: 'GET', uri: 'http://rss.uol.com.br/feed/economia.xml'};
+const request = require('request');
+const parser = require('fast-xml-parser');
+const json2csv = require('csvjson-json2csv');
 
-var request = require('request');
 request(requestOptions, function (error, response, body) {
+  
   var json = parser.parse(body);
   var newsArray = json['rss']['channel']['item'];
+  
   newsArray.forEach(function(news){ 
     delete news.link; 
     delete news.pubDate;
     news.description = news.description.replace(/<img .*?>/g, '');
   });
+
+  var newsCsv = json2csv(newsArray);
+  console.log(newsCsv);
+
 });
+
