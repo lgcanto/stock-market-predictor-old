bd0ec7e88c7baab0771c346bfb3c97db1765860f
Lucas Canto <lucas.canto@siemens.com>
2019-06-24 00:13:34 -0300

    adding modelling for VALE3

--- VALE3.py
+++ VALE3.py
@@ -1,21 +1,56 @@
 import main
 import pandas as pd # processamento de dados, leitura de arquivos .csv
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 acaoEmpresa = 'VALE3'
 nomesEmpresa = main.df_empresas[main.df_empresas.code == acaoEmpresa].names.max()
 
-# filtrando índices para obtermos apenas o papel em questão e ordenando por data
+# filtrando indices para obtermos apenas o papel em questao e ordenando por data
 df_bovespa = main.df_bovespa[main.df_bovespa.codneg.str.strip() == acaoEmpresa]
 df_bovespa.sort_values('date')
-# criando coluna de classificação
-df_bovespa['class'] = df_bovespa.apply(lambda row: main.defineClass(row['open'], row['close']), axis=1)
+# criando coluna de classificacao
+df_bovespa['classification'] = df_bovespa.apply(lambda row: main.defineClass(row['open'], row['close']), axis=1)
 
-# filtrando notícias contendo nomes associadas à ação e ordenando por data
-df_artigos = main.df_artigos[main.df_artigos.title.str.contains(nomesEmpresa)]
+# filtrando noticias contendo nomes associadas a acao, filtrando caracteres e ordenando por data
+df_artigos = main.df_artigos[main.df_artigos.news.str.contains(nomesEmpresa)]
+df_artigos['news'] = df_artigos.apply(lambda row: main.applyRegexp(row['news']), axis=1)
 df_artigos.sort_values('date')
 # criando coluna de data de efeito
 df_artigos['effect_date'] = df_artigos.apply(lambda row: main.defineEffectDate(row['date']), axis=1)
-# renomeando coluna de data para facilitar junção de datasets
+# renomeando coluna de data para facilitar juncao de datasets
 df_bovespa = df_bovespa.rename(columns={'date': 'effect_date'})
 
+# juncao do dataset de noticias com dataset de indices; criacao do dataset
 df = pd.merge(df_artigos, df_bovespa, on='effect_date', how='left')
+train_unused_columns = ['date', 'effect_date', 'codneg', 'company', 'open', 'close']
+df.drop(train_unused_columns, inplace=True, axis=1)
+
+X_verbal = df.news.tolist()
+Y = df.classification.tolist()
+
+# transformando a contagem de palavras em features
+cv = CountVectorizer(binary=True)
+cv.fit(X_verbal)
+X = cv.transform(X_verbal)
+
+# dividindo dataset entre treino (75%) e teste (25%)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, train_size = 0.75
+)
+
+# verificando melhor valor de C (hiperparametro de regularizacao)
+for c in [0.01, 0.05, 0.25, 0.5, 1]:
+    
+    lr = LogisticRegression(C=c)
+    lr.fit(X_train, Y_train)
+    print ("Acur�cia para C=%s: %s" 
+           % (c, accuracy_score(Y_test, lr.predict(X_test))))
+    
+# definicao do modelo final
+modelo = LogisticRegression(C=0.5)
+modelo.fit(X_train, Y_train)
+print ("Acur�cia final: %s" 
+       % accuracy_score(Y_test, modelo.predict(X_test)))
--- main.py
+++ main.py
@@ -1,44 +1,60 @@
-import numpy as np # algébra linear
+import re
+import numpy as np # algebra linear
 import pandas as pd # processamento de dados, leitura de arquivos .csv
+from unicodedata import normalize
 
-## Extração de Dados
+# expressoes regulares
+REGEXP_ESPACO = re.compile("[.;:!\'?,\"()\[\]]")
+REGEXP_VAZIO = re.compile("(\-)|(\/)")
+
+## Extracao de Dados
 df_empresas = pd.read_csv('datasets/company-codes.csv')
 df_bovespa = pd.read_csv('datasets/kaggle/bovespa.csv')
 df_artigos = pd.read_csv('datasets/kaggle/articles.csv')
 
-## Funções
+## Funcoes
 
-# denifição de estado de fechamento diário do papel
+# definicao de estado de fechamento diario do papel
 def defineClass (open, close):
     if close > open:
-        return 1 # ocorreu valorização
+        return 1 # ocorreu valorizacao
     if open > close:
-        return -1 # ocorreu desvalorização
+        return -1 # ocorreu desvalorizacao
     else:
-        return 0 # não houve mudança de preço
+        return 0 # nao houve mudanca de preco
     
-# definição de data de efeito da notícia (próxima data de funcionamento da B3 após o lançamento da notícia)
+# definicao de data de efeito da noticia (proxima data de funcionamento da B3 aos o lancamento da noti�cia)
 def defineEffectDate (date):
     effectDate = date
     while (df_bovespa[df_bovespa.date == effectDate].size < 1):
         effectDate = effectDate + 1
     return effectDate
 
+# remocao de caracteres desnecessarios do texto a ser processado
+def applyRegexp (text):
+    finalText = text
+    finalText = REGEXP_ESPACO.sub(" ", finalText.lower().strip())
+    finalText = REGEXP_VAZIO.sub("", finalText)
+    finalText = normalize('NFKD', finalText).encode('ASCII', 'ignore').decode('ASCII')
+    return finalText
+
 ## Processamento de Dados
 
-# removendo colunas desnecessárias do dataset de notícias
+# removendo colunas desnecessarias do dataset de noti�cias
 artigos_unused_columns = ['text', 'category', 'subcategory', 'link']
 df_artigos.drop(artigos_unused_columns, inplace=True, axis=1)
-# formatando coluna de data da mesma forma que o dataset de índices
+# renomeando coluna com do atributo escolhido como o texto a ser analisado
+df_artigos.rename(columns={'title': 'news'}, inplace=True)
+# formatando coluna de data da mesma forma que o dataset de i�ndices
 df_artigos['date'] = df_artigos.apply(lambda row: np.int64(row['date'].replace('-', '')), axis=1)
 
-# removendo colunas desnecessárias do dataset de índices
+# removendo colunas desnecessarias do dataset de i�ndices
 bovespa_unused_columns = ['TypeReg', 'BDICode', 'MarketType', 'Spec', 'Prazot', 'Currency', 'Max', 'Min', 'Med', 'Preofc', 'Preofv', 'Totneg', 'Quatot']
 df_bovespa.drop(bovespa_unused_columns, inplace=True, axis=1)
 df_bovespa.columns = map(str.lower, df_bovespa.columns)
-# filtrando índices para obtermos apenas índices de empresas do índice Bovespa
+# filtrando indices para obtermos apenas indices de empresas do i�ndice Bovespa
 df_bovespa = df_bovespa[df_bovespa.codneg.str.strip().isin(df_empresas.code)]
-# removendo janela de tempo inferior que não existe no dataset de notícias
+# removendo janela de tempo inferior que nao existe no dataset de noti�cias
 df_bovespa = df_bovespa[df_bovespa.date >= df_artigos.date.min()]
 
 # criando coluna de data de efeito
